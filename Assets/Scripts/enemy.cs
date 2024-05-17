51d355f3cf3d0a59ade3e1ac28b56318f341f26a
Msheph1 <max.r.shep@gmail.com>
2024-05-17 01:36:08 -0500

    new scene for testing

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class enemy : MonoBehaviour
 {
     public float speed = 5f;
+ 
 
     [SerializeField] private Transform target;
+
+
+    
 
     // Update is called once per frame
     void Update()
@@ -15,4 +20,6 @@ public class enemy : MonoBehaviour
         Vector3 targetPosition = target.position;
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
+
+
 }
